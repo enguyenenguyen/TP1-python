5705a519e9341c28affebd8d0ea8a5a42994a5c6
Esteban Nguyen <esteban.nguyen@uha.fr>
2023-11-16 11:50:32 +0100

    Initial commit

---
+++ TP1/Exercice 1.py
@@ -0,0 +1,18 @@
+nom = "toto"
+prenom = "titi"
+math = 12.5
+anglais = 12.5
+info = 12.5
+promotion = 2023
+moy = (math + anglais + info)/3
+print (f"Type de 'nom': {type(nom)}")
+print (f"Type de 'prenom': {type(prenom)}")
+print (f"Type de 'math': {type(math)}")
+print (f"Type de 'anglais': {type(anglais)}")
+print (f"Type de 'info': {type(info)}")
+print (f"Type de 'promotion': {type(promotion)}")
+print (f"Type de 'moy': {type(moy)}")
+print (f"L'étudiant {nom} {prenom} de la promotion {promotion} a une moyenne de {moy:.1f}")
+
+
+
---
+++ TP1/Exercice 2.py
@@ -0,0 +1,5 @@
+jour = 16
+heure = 10
+minute = 50
+min = jour*1440 + heure*60 + minute
+print (f"Depuis le début de l'année, il s'est écoulé {min} minutes")
